1b44e3ef837d4ae42294ad5cdc72ba1a1a6b2e34
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-11-15 13:12:40 +0000

    tutor

--- assistant-function-calling/readline-test.js
+++ assistant-function-calling/readline-test.js
@@ -11,31 +11,6 @@ async function askRLineQuestion(question) {
   });
 }
 
-async function displayQuiz(name, questions) {
-  // This function will take the name of the quiz and the questions and it will display them to the user. 
-  // It will then wait for the user to answer the questions 
-  // and then it will return the responses.
-  console.log(`\n${name}\n`);
-
-  const responses = {};
-
-  for (const question of questions) {
-    const questionText = question.question_text;
-    const questionType = question.question_type;
-    const choices = question.choices;
-
-    console.log(`\n${questionText}\n`);
-
-    if (questionType === "MULTIPLE_CHOICE") {
-      console.log(`\n${choices.join("\n")}\n`);
-    }
-
-    const response = await askRLineQuestion("Your response: \n");
-
-    responses[questionText] = response;
-  }
-}
-
 async function main() {
   let continueConversation = true;
   let answers = [];
