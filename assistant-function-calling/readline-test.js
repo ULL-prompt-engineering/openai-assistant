21abd10150fba16471257d9f18e6a00bbf720997
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-11-15 13:12:13 +0000

    tutor

--- assistant-function-calling/readline-test.js
+++ assistant-function-calling/readline-test.js
@@ -38,15 +38,17 @@ async function displayQuiz(name, questions) {
 
 async function main() {
   let continueConversation = true;
+  let answers = [];
 
-  while (continueConversation) {
-    const answer = await askRLineQuestion("What is a food you like? ")
-    console.log(`Oh, so you like ${answer}`)
+  do  {
+    let answer = await askRLineQuestion("What is a food you like? ");
+    answers.push(answer.trim());
+    console.log(`Oh, so you like: ${answers.join(", ")}`);
     const continueAsking = await askRLineQuestion(
       "Do you want to keep having a conversation? (yes/no) ",
     );
     continueConversation = continueAsking.toLowerCase().includes("y");
-  }
+  } while (continueConversation);
 
   console.log("Bye!\n");
   readline.close();
